7c5140e47a0f846e4b8b16aeac1f61cc86050ce2
blackwellsmith <blackwellsmithiv@gmail.com>
2020-02-18 14:21:17 -0500

    added redux react-redux redux-thunk and im ported them into my index.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from 'react';
-
 import './App.css';
 
 class App extends React.Component {
-  
 
   render(){
   return (
-    <div className="App">
-      <h1>get heard</h1>
-    </div>
+    "get heard"
   );
   
   }
