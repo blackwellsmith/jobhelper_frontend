dd8f61e03d689da995aab367a025f60f1a27bf43
blackwellsmith <blackwellsmithiv@gmail.com>
2020-02-18 17:30:50 -0500

    made current user in actions folder and reducers folder made SET_CURRENT_USER action

--- src/index.js
+++ src/index.js
@@ -1,19 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
-import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
-import users from './reducers/users.js'
+import store from './store.js'
 
 
-const reducer = combineReducers({
-    users: users
-})
 
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSIONS_COMPOSE__ || compose;
 
-const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 
