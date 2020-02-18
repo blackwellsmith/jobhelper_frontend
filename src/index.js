4272f579a4f7b16d88364aea92fe0a9f9dde79a1
blackwellsmith <blackwellsmithiv@gmail.com>
2020-02-18 14:37:09 -0500

    set up requirements in index.js for reudx for moving into proper area later

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { createStore, applyMiddleware } from "redux"
-import { thunk } from "redux-thunk"
-import { provider } from 'react-redux'
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
 
+const users = () => {}
+const reducer = combineReducers({
+    users
+})
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSIONS_COMPOSE__ || compose;
+
+const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 
