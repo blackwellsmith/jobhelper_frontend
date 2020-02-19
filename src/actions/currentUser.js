2b04e5228403dc245f33c050670b3cf9783601c4
blackwellsmith <blackwellsmithiv@gmail.com>
2020-02-19 15:29:24 -0500

    in actions currentUser created getCurrentUser fetch request and imported connect and getCurrentUser into app.js

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -27,3 +27,24 @@ export const login = credentials => {
         .catch(console.log)
     }
 }
+
+export const getCurrentUser = () => {
+    return dispatch => {
+        return fetch("http://localhost:3000/get_current_user", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            
+        })
+            .then(r => r.json())
+            .then(user => {
+                if (user.error) {
+                    alert(user.error)
+                } else {
+                    dispatch(setCurrentUser(user)) 
+                }
+            })
+        .catch(console.log)
+    }
+}
