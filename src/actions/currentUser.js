7f9ea15012908f322f9313ac9035f1c5dc2fc71e
blackwellsmith <blackwellsmithiv@gmail.com>
2020-02-19 15:02:09 -0500

    turned my fetch response into json then add if statement checking for errors or dispatching setCurrentUser

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -1,4 +1,4 @@
-export const setCurrent = user => {
+export const setCurrentUser = user => {
     return {
         type: "SET_CURRENT_USER",
         user
@@ -9,12 +9,21 @@ export const setCurrent = user => {
 export const login = credentials => {
     console.log("wtf", credentials)
     return dispatch => {
-        return fetch("http://localhost3000/login", {
+        return fetch("http://localhost:3000/login", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
             },
-            body: JSON.stringify()
+            body: JSON.stringify(credentials)
         })
+            .then(r => r.json())
+            .then(user => {
+                if (user.error) {
+                    alert(user.error)
+                } else {
+                    dispatch(setCurrentUser(user)) 
+                }
+            })
+        .catch(console.log)
     }
 }
