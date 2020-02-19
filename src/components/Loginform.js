0e3442810c2b5c36a4afe6275f10efc16a88539f
blackwellsmith <blackwellsmithiv@gmail.com>
2020-02-18 19:45:29 -0500

    mapStateToProps in LoginForm and added place holder values in input fields

--- src/components/Loginform.js
+++ src/components/Loginform.js
@@ -1,12 +1,24 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
-const Login = () => {
+
+const Login = (props) => {
     return (
         <form onSubmit={undefined}>
-            <input placeholder="email" value={undefined} email="email" type='text' onChange={undefined} />
-            <input placeholder="password" value={undefined} password="password" type='text' onChange={undefined} />
+            <input placeholder="email" value={props.email} email="email" type='text' onChange={undefined} />
+            <input placeholder="password" value={props.password} password="password" type='text' onChange={undefined} />
             <input type="submit" value="Log in" />
         </form>
     )
 }
-export default Login
+const mapStateToProps = state => {
+    return {
+        email: state.loginForm.email,
+        password: state.loginForm.password
+    }
+}
+const dispatchStateToProps = () => {
+    
+}
+
+export default connect(mapStateToProps, dispatchStateToProps)(Login)
