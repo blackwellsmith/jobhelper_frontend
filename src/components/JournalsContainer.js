7fbcb2a43c3fdd6dbfe4c2e825f0520f24de3643
blackwellsmith <blackwellsmithiv@gmail.com>
2020-02-22 21:03:48 -0500

    added strftime with yarn and fiddled with css to center align button

--- src/components/JournalsContainer.js
+++ src/components/JournalsContainer.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux'
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
 import Popover from 'react-bootstrap/Popover'
 import Button from 'react-bootstrap/Button'
+import ButtonToolbar from 'react-bootstrap/ButtonToolbar'
 
 
 const Journals = ({ journalIndex }) => {
@@ -10,9 +11,9 @@ const Journals = ({ journalIndex }) => {
     return (
         <div className="JournalsContainer">
             <h1>Your Journal Entries</h1>
-            <ul>
+              <ButtonToolbar className="bt">
                 {journalIndex.map(journal => (
-                <li>
+                
                   <OverlayTrigger
                   trigger="click"
                   key='bottom'
@@ -21,19 +22,21 @@ const Journals = ({ journalIndex }) => {
                     <Popover id='popover-positioned-bottom'>
                       <Popover.Title as="h3"><stron>Notes:</stron>{journal.entry}</Popover.Title>
                       <Popover.Content>
-                        <strong>People you connected with:</strong> {journal.meetups}
-                        <strong>Skills improvement:</strong> {journal.study}
-                        <strong>Jobs applied for:</strong> {journal.applications}
-                        <strong>Goals set:</strong> {journal.goals}                   
+                        <strong>People you connected with: </strong> {journal.meetups}
+                        <strong> - Skills improvement: </strong> {journal.study}
+                        <strong> - Jobs applied for: </strong> {journal.applications}
+                        <strong> - Goals set: </strong> {journal.goals}                   
                       </Popover.Content>
                     </Popover>
                   }>
-                            <Button variant="secondary">{journal.created_at}</Button>
+                    <Button className='button' variant="primary">{journal.created_at}</Button>
+                        
                   </OverlayTrigger>
-                </li>        
+                    
+                    
               ))}
-            </ul>
             
+            </ButtonToolbar> 
         </div>
     )
 }
