7b8010be066e263abcd099db795330ef9b089e04
blackwellsmith <blackwellsmithiv@gmail.com>
2020-02-25 12:07:41 -0500

    added new text to the logged in carousel job hunt tips

--- src/components/CarouselLoggedIn.js
+++ src/components/CarouselLoggedIn.js
@@ -18,8 +18,9 @@ border: 2.5px solid white;
 const Page = styled.div`
 
 width: 100%;
-height: 140px;
+height: 180px;
 padding: 30px;
+padding-bottom: 30px;
 `
 
 
@@ -36,13 +37,14 @@ class CarouselComponentLoggedIn extends React.Component {
         return (
             <Wrapper>
                 <Slider {...settings}>
-                    <Page><h2 style={{ color: 'white' }}>So yo want a job as a  <span style={{ color: 'darkblue' }}>{this.props.currentJobGoal.name}</span>.</h2></Page>
-                    <Page><h2 style={{ color: 'white' }}> Keeping organized and on track can be a struggle </h2></Page>
-                    <Page><h2 style={{ color: 'white' }}> A job search journal is essential an tool for our busy world </h2></Page>
-                    <Page><h2 style={{ color: 'white' }}> Heard allows you to keep track of your contacts and daily job search activities</h2></Page>
-                    <Page><h2 style={{ color: 'white' }}> Heard is free all we ask is that you use it everyday during your job search</h2></Page>
-                    <Page><h2 style={{ color: 'white' }}> Heard won't get the job for you but it will help you get in the right mindset</h2></Page>                   
-                    <Page><h2 style={{ color: 'white' }}> Thank you for your time! Have a great day!</h2></Page>
+                    <Page><h2 style={{ color: 'white' }}> Here is a list of habits for your successful job search. </h2></Page>
+                    <Page><h2 style={{ color: 'white' }}> Keep a positive attitude. </h2></Page>
+                    <Page><h2 style={{ color: 'white' }}> Use Heard everyday, keeping track of what you do can keep you on headed down the correct path. </h2></Page>
+                    <Page><h2 style={{ color: 'white' }}> It's not what you know it's who you know, fill out your contacts list and use it.</h2></Page>
+                    <Page><h2 style={{ color: 'white' }}> Buy someone coffee 3 times a week, get out talk to people you never know what can happen. Get seen be Heard.</h2></Page>
+                    <Page><h2 style={{ color: 'white' }}> Keep track of what positions you have applied in your journal, use that information for follow up calls.</h2></Page>                   
+                    <Page><h2 style={{ color: 'white' }}> Always work to improve your professional skills. What your reading or doing is a great way to show your willing to learn.</h2></Page>
+                    <Page><h2 style={{ color: 'white' }}> We believe staying organized and being positive during your job search will return the best possible results.</h2></Page>
                 </Slider>
               
             </Wrapper>     
