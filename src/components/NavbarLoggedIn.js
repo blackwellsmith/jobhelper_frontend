6c8c00a26479fdccc457e26c29030df84789710e
blackwellsmith <blackwellsmithiv@gmail.com>
2020-02-27 16:17:22 -0500

    added css for navbar height

--- src/components/NavbarLoggedIn.js
+++ src/components/NavbarLoggedIn.js
@@ -16,22 +16,22 @@ const NavbarLoggedIn = (props) => {
                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                <Navbar.Collapse id="responsive-navbar-nav">
                <Nav className="mr-auto">
-               <Nav.Link href="/">Home</Nav.Link>
-               <Nav.Link href="/journals">Daily Journal</Nav.Link>
-               <Nav.Link href="/contacts">Contacts</Nav.Link>        
+               <Nav.Link href="/">Home </Nav.Link>
+               <Nav.Link href="/journals">Daily Journal </Nav.Link>
+               <Nav.Link href="/contacts">Contacts </Nav.Link>        
                <NavDropdown title="Write" id="collasible-nav-dropdown">
-                <NavDropdown.Item href="/journal">Journal Entry</NavDropdown.Item>
-                <NavDropdown.Item href="/contact">Add Contacts</NavDropdown.Item>
-                <NavDropdown.Item href="/job">Desired Position</NavDropdown.Item>
-                        </NavDropdown>
-                        <Navbar.Text>
-                           <div> Signed in as: {props.currentUser.name}   </div>
-                    </Navbar.Text>       
+               <NavDropdown.Item href="/journal">Journal Entry</NavDropdown.Item>
+               <NavDropdown.Item href="/contact">Add Contacts</NavDropdown.Item>
+               <NavDropdown.Item href="/job">Desired Position</NavDropdown.Item>
+               </NavDropdown>
+               <Navbar.Text>
+               <div>       Signed in as: {props.currentUser.name}   </div>
+               </Navbar.Text> 
                </Nav>
-                    <Nav>
-                    
-                    </Nav>
-                    <Nav>
+               <Nav>
+               
+               </Nav>
+               <Nav>
                <Form inline>
                <Logout />
                </Form>
