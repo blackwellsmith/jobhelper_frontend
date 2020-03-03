0810a6d513857e06754534155a453298f3a4087d
blackwellsmith <blackwellsmithiv@gmail.com>
2020-03-03 08:30:27 -0500

    built job goal card for job goal present next job goal card with goal no created yet

---
+++ src/components/JobCardNoGoal.js
@@ -0,0 +1,24 @@
+import React from "react"
+import Card from 'react-bootstrap/Card'
+
+const JobCardNoGoal = (props) => {
+    {console.log(props.currentJobGoal)}
+    return (
+       
+                    <div className="badgecontainer">
+                        <Card body bg="secondary" text="white" border="primary">
+                            <Card.Header>Dream job</Card.Header>
+                            <Card.Body>
+                                <Card.Title>Title: {props.currentJobGoal.name}</Card.Title>
+                                <Card.Title>Location: {props.currentJobGoal.location}</Card.Title>
+                                <Card.Title>Compensation: {props.currentJobGoal.pay}</Card.Title>
+                                <Card.Text>
+                                    Update your Dream Job below.
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </div>
+    )
+}
+
+export default (JobCardNoGoal)
